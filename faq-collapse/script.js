100e31567cb5770fc3df1d8218b603a6b7392032
Praneeth <praneeth001@gmail.com>
2023-08-22 10:33:55 +0530

    edits

--- faq-collapse/script.js
+++ faq-collapse/script.js
@@ -1,5 +1,6 @@
 const toggles = document.querySelectorAll('.faq-toggle')
 
+// access the parentNode classlist and toggle the active class
 toggles.forEach(toggle => {
     toggle.addEventListener('click', () => {
         toggle.parentNode.classList.toggle('active')
